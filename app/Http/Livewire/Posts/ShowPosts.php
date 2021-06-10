1101563b2e05d35484aeb46af4797c221129e29b
Javad Jafariniya <javad1994ma@gmail.com>
2021-06-10 15:34:04 +0430

    part 10 saw

---
+++ app/Http/Livewire/Posts/ShowPosts.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Livewire\Posts;
+
+use Livewire\Component;
+
+class ShowPosts extends Component
+{
+    public function render()
+    {
+        return <<<'blade'
+            <div>
+                {{-- Because she competes with no one, no one can compete with her. --}}
+            </div>
+        blade;
+    }
+}
